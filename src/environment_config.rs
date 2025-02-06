13895684ea79a6b1e59b8bb6f54dbd027fc7f04a
0xPraedico <meowmeowcapital@gmail.com>
2025-02-06 18:29:25 +0100

    update

--- src/environment_config.rs
+++ src/environment_config.rs
@@ -1,15 +1,87 @@
+use std::{collections::{HashMap, HashSet}, hash::RandomState, sync::Arc};
+
 
 pub type Pubkey = [u8; 32];
 /// Size of a hash in bytes.
 pub const HASH_BYTES: usize = 32;
+#[derive(Clone, Copy, Default, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
 pub struct Hash(pub(crate) [u8; HASH_BYTES]);
 
+pub type SlotHash = (u64, Hash);
 
-#[derive(Debug, Clone, Eq, PartialEq)]
+pub type StakeHistoryInner = StakeHistory;
+#[derive(PartialEq, Eq, Debug, Default)]
+pub struct SlotHashes(Vec<SlotHash>);
+
+
+
+#[derive(Default, Clone, PartialEq, Eq, Debug)]
+pub struct StakeHistory(Arc<StakeHistoryInner>);
+
+#[derive(Debug, Clone)]
 pub struct FeatureSet {
-    pub active: AHashMap<Pubkey, u64>,
-    pub inactive: AHashSet<Pubkey>,
+    pub active: AHashMap<Pubkey, u64>, // randomness
+    pub inactive: AHashSet<Pubkey>,  // randomness 
 }
+
+/// A [`HashMap`](std::collections::HashMap) using [`RandomState`](crate::RandomState) to hash the items.
+/// (Requires the `std` feature to be enabled.)
+#[derive(Clone, Debug)]
+pub struct AHashMap<K, V, S = RandomState>(HashMap<K, V, S>);
+
+/// A [`HashSet`](std::collections::HashSet) using [`RandomState`](crate::RandomState) to hash the items.
+/// (Requires the `std` feature to be enabled.)
+#[derive(Clone, Debug)]
+pub struct AHashSet<T, S = RandomState>(HashSet<T, S>);
+
+#[derive(Debug,Clone, Default, PartialEq, Eq)]
+pub struct Fees {
+    pub fee_calculator: FeeCalculator,
+}
+
+#[derive(Default, PartialEq, Eq, Clone, Copy, Debug)]
+#[cfg_attr(feature = "serde", serde(rename_all = "camelCase"))]
+pub struct FeeCalculator {
+    /// The current cost of a signature.
+    ///
+    /// This amount may increase/decrease over time based on cluster processing
+    /// load.
+    pub lamports_per_signature: u64,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct RecentBlockhashes(Vec<Entry>);
+
+#[derive(Clone, Debug, Default, PartialEq, Eq)]
+pub struct Entry {
+    pub blockhash: Hash,
+    pub fee_calculator: FeeCalculator,
+}
+
+#[derive(Default, Clone, Debug)]
+pub struct SysvarCache {
+    // full account data as provided by bank, including any trailing zero bytes
+    clock: Option<Vec<u8>>,
+    epoch_schedule: Option<Vec<u8>>,
+    epoch_rewards: Option<Vec<u8>>,
+    rent: Option<Vec<u8>>,
+    slot_hashes: Option<Vec<u8>>,
+    stake_history: Option<Vec<u8>>,
+    last_restart_slot: Option<Vec<u8>>,
+
+    // object representations of large sysvars for convenience
+    // these are used by the stake and vote builtin programs
+    // these should be removed once those programs are ported to bpf
+    slot_hashes_obj: Option<Arc<SlotHashes>>,
+    stake_history_obj: Option<Arc<StakeHistory>>,
+
+    // deprecated sysvars, these should be removed once practical
+    #[allow(deprecated)]
+    fees: Option<Fees>,
+    #[allow(deprecated)]
+    recent_blockhashes: Option<RecentBlockhashes>,
+}
+
 pub struct EnvironmentConfig<'a> {
     pub blockhash: Hash,
     pub blockhash_lamports_per_signature: u64,
--- src/program_cache_for_tx_batch.rs
+++ src/program_cache_for_tx_batch.rs
@@ -1,13 +1,122 @@
 use solana_sbpf::{
     program::BuiltinProgram,
     elf::Executable};
-use std::{cell::RefCell, pin::Pin, rc::Rc, sync::Arc};
+use std::{cell::RefCell, collections::{BTreeMap, HashMap}, pin::Pin, rc::Rc, sync::Arc};
 
 use crate::InvokeContext;
 
 pub type ProgramRuntimeEnvironment = Arc<BuiltinProgram<InvokeContext<'static>>>;
+pub type Pubkey = [u8; 32];
 
+/// Syscall function without context
+pub type BuiltinFunction<C> = fn(*mut EbpfVm<C>, u64, u64, u64, u64, u64);
 
+#[repr(C)]
+pub struct EbpfVm<'a, C: ContextObject> {
+    /// Needed to exit from the guest back into the host
+    pub host_stack_pointer: *mut u64,
+    /// The current call depth.
+    ///
+    /// Incremented on calls and decremented on exits. It's used to enforce
+    /// config.max_call_depth and to know when to terminate execution.
+    pub call_depth: u64,
+    /// Pointer to ContextObject
+    pub context_object_pointer: &'a mut C,
+    /// Last return value of instruction_meter.get_remaining()
+    pub previous_instruction_meter: u64,
+    /// Outstanding value to instruction_meter.consume()
+    pub due_insn_count: u64,
+    /// CPU cycles accumulated by the stop watch
+    pub stopwatch_numerator: u64,
+    /// Number of times the stop watch was used
+    pub stopwatch_denominator: u64,
+    /// Registers inlined
+    pub registers: [u64; 12],
+    /// ProgramResult inlined
+    pub program_result: ProgramResult,
+    /// MemoryMapping inlined
+    pub memory_mapping: MemoryMapping<'a>,
+    /// Stack of CallFrames used by the Interpreter
+    pub call_frames: Vec<CallFrame>,
+    /// Loader built-in program
+    pub loader: Arc<BuiltinProgram<C>>,
+    /// TCP port for the debugger interface
+    #[cfg(feature = "debugger")]
+    pub debug_port: Option<u16>,
+}
+
+/// Runtime context
+pub trait ContextObject {
+    /// Called for every instruction executed when tracing is enabled
+    fn trace(&mut self, state: [u64; 12]);
+    /// Consume instructions from meter
+    fn consume(&mut self, amount: u64);
+    /// Get the number of remaining instructions allowed
+    fn get_remaining(&self) -> u64;
+}
+
+/// Represents the interface to a fixed functionality program
+#[derive(Eq)]
+pub struct BuiltinProgram<C: ContextObject> {
+    /// Holds the Config if this is a loader program
+    config: Option<Box<Config>>,
+    /// Function pointers by symbol with sparse indexing
+    sparse_registry: FunctionRegistry<BuiltinFunction<C>>,
+    /// Function pointers by symbol with dense indexing
+    dense_registry: FunctionRegistry<BuiltinFunction<C>>,
+}
+
+/// Holds the function symbols of an Executable
+#[derive(Debug, PartialEq, Eq)]
+pub struct FunctionRegistry<T> {
+    pub(crate) map: BTreeMap<u32, (Vec<u8>, T)>,
+}
+/// VM configuration settings
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Config {
+    /// Maximum call depth
+    pub max_call_depth: usize,
+    /// Size of a stack frame in bytes, must match the size specified in the LLVM BPF backend
+    pub stack_frame_size: usize,
+    /// Enables the use of MemoryMapping and MemoryRegion for address translation
+    pub enable_address_translation: bool,
+    /// Enables gaps in VM address space between the stack frames
+    pub enable_stack_frame_gaps: bool,
+    /// Maximal pc distance after which a new instruction meter validation is emitted by the JIT
+    pub instruction_meter_checkpoint_distance: usize,
+    /// Enable instruction meter and limiting
+    pub enable_instruction_meter: bool,
+    /// Enable instruction tracing
+    pub enable_instruction_tracing: bool,
+    /// Enable dynamic string allocation for labels
+    pub enable_symbol_and_section_labels: bool,
+    /// Reject ELF files containing issues that the verifier did not catch before (up to v0.2.21)
+    pub reject_broken_elfs: bool,
+    /// Ratio of native host instructions per random no-op in JIT (0 = OFF)
+    pub noop_instruction_rate: u32,
+    /// Enable disinfection of immediate values and offsets provided by the user in JIT
+    pub sanitize_user_provided_values: bool,
+    /// Avoid copying read only sections when possible
+    pub optimize_rodata: bool,
+    /// Use aligned memory mapping
+    pub aligned_memory_mapping: bool,
+    /// Allowed [SBPFVersion]s
+    pub enabled_sbpf_versions: std::ops::RangeInclusive<SBPFVersion>,
+}
+/// Defines a set of sbpf_version of an executable
+#[derive(Debug, PartialEq, PartialOrd, Eq, Clone, Copy)]
+pub enum SBPFVersion {
+    /// The legacy format
+    V0,
+    /// SIMD-0166
+    V1,
+    /// SIMD-0174, SIMD-0173
+    V2,
+    /// SIMD-0178, SIMD-0179, SIMD-0189
+    V3,
+    /// Used for future versions
+    Reserved,
+}
 
 /// The owner of a programs accounts, thus the loader of a program
 #[derive(Default, Clone, Copy, PartialEq, Eq, Debug)]
@@ -41,6 +150,7 @@ pub enum ProgramCacheEntryType {
     /// A built-in program which is not stored on-chain but backed into and distributed with the validator
     Builtin(BuiltinProgram<InvokeContext<'static>>),
 }
+#[derive(Debug, Default)]
 pub struct ProgramCacheEntry {
     /// The program of this entry
     pub program: ProgramCacheEntryType,
